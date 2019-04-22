ae2ed86fc387c6ff52e2168f6e6924af8d2601b4
593979591@qq.com <zz789789>
2019-04-22 15:38:59 +0800

    修改状态栏

--- Core/src/main/java/com/hqq/core/utils/statusbar/StatusBarManager.java
+++ Core/src/main/java/com/hqq/core/utils/statusbar/StatusBarManager.java
@@ -54,6 +54,7 @@ public class StatusBarManager {
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             activity.getWindow().setStatusBarColor(Color.TRANSPARENT);
 
