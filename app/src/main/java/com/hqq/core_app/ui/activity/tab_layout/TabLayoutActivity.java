46be4a69a7b449946872d794bd918fd0ed8a3bbe
593979591@qq.com <zz789789>
2019-06-06 18:03:58 +0800

    添加一些笔记

---
+++ app/src/main/java/com/hqq/core_app/ui/activity/tab_layout/TabLayoutActivity.java
@@ -0,0 +1,26 @@
+package com.hqq.core_app.ui.activity.tab_layout;
+
+import com.hqq.core.ui.BaseActivity;
+import com.hqq.core_app.R;
+
+/**
+ * @Author : huangqiqiang
+ * @Package : com.hqq.core_app.ui.activity.tab_layout
+ * @FileName :   TabLayoutActivity
+ * @Date : 2019/6/6 0006  下午 6:00
+ * @Email :  qiqiang213@gmail.com
+ * @Descrive :
+ */
+public class TabLayoutActivity extends BaseActivity {
+
+    @Override
+    public int setViewId() {
+        return R.layout.activity_tab_layout;
+    }
+
+
+    @Override
+    public void initView() {
+
+    }
+}
