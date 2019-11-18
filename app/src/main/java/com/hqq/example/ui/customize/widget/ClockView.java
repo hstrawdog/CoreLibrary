51919edde025d9df118f20280661ae7c2019ab3d
593979591@qq.com <zz789789>
2019-11-18 10:50:36 +0800

    添加时分秒

--- app/src/main/java/com/hqq/example/ui/customize/widget/ClockView.java
+++ app/src/main/java/com/hqq/example/ui/customize/widget/ClockView.java
@@ -11,6 +11,7 @@ import androidx.annotation.Nullable;
 
 import com.hqq.core.utils.log.LogUtils;
 
+import java.security.acl.LastOwnerException;
 import java.util.Calendar;
 
 /**
@@ -48,32 +49,58 @@ public class ClockView extends View {
         Paint paint = drawBg(canvas);
 
         paint.setColor(Color.RED);
-        int hour = Calendar.getInstance().get(Calendar.HOUR);
 
-        int second = Calendar.getInstance().get(Calendar.SECOND);
-        LogUtils.e("--------------------   "+second);
-        canvas.save();
-        canvas.rotate(6 * second, 0, getWidth() / 2);
-        canvas.drawLine(0, getHeight() / 2 + 30, 0, getHeight() / 2 - 120, paint);
+        drawSecond(canvas, paint);
+
+        drawMinute(canvas, paint);
+        drawHour(canvas, paint);
 
         postInvalidateDelayed(1000);
     }
+
+    private void drawMinute(Canvas canvas, Paint paint) {
+        int minute = Calendar.getInstance().get(Calendar.MINUTE);
+        canvas.save();
+        canvas.rotate(6 * minute, 0, getWidth() / 2);
+        canvas.drawLine(0, getHeight() / 2 + 30, 0, getHeight() / 2 - 80, paint);
+        canvas.restore();
+    }
+
+    private void drawHour(Canvas canvas, Paint paint) {
+        int hour = Calendar.getInstance().get(Calendar.HOUR);
+
+        LogUtils.e("--------------------   " + hour);
+        canvas.save();
+        canvas.rotate(30 * hour, 0, getWidth() / 2);
+        canvas.drawLine(0, getHeight() / 2 + 30, 0, getHeight() / 2 - 60, paint);
+        canvas.restore();
+    }
+
+    private void drawSecond(Canvas canvas, Paint paint) {
+        int second = Calendar.getInstance().get(Calendar.SECOND);
+        LogUtils.e("--------------------   " + second);
+        canvas.save();
+        canvas.rotate(6 * second, 0, getWidth() / 2);
+        canvas.drawLine(0, getHeight() / 2 + 30, 0, getHeight() / 2 - 180, paint);
+        canvas.restore();
+    }
+
 
     private Paint drawBg(Canvas canvas) {
         Paint paint = new Paint();
         paint.setColor(Color.GRAY);
         paint.setStrokeWidth(5.0f);
         paint.setAntiAlias(true);//设置抗锯齿
-        canvas.save();
         canvas.translate(getWidth() / 2, 0);
+        canvas.save();
         for (int i = 0; i < 4; i++) {
             canvas.drawLine(0, 0, 0, 100, paint);
             canvas.rotate(90, 0, getWidth() / 2);
         }
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 12; i++) {
             canvas.drawLine(0, 0, 0, 40, paint);
-            canvas.rotate(45, 0, getWidth() / 2);
+            canvas.rotate(30, 0, getWidth() / 2);
         }
         for (int i = 0; i < 60; i++) {
             canvas.drawLine(0, 0, 0, 3, paint);
@@ -81,6 +108,8 @@ public class ClockView extends View {
         }
 
         canvas.drawCircle(0, getHeight() / 2, 20, paint);
+        canvas.restore();
+
         return paint;
     }
 }
