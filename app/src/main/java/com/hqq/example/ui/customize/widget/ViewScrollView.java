244ff7ec3a3ee8985d257a7635df5656e44b35d9
593979591@qq.com <zz789789>
2019-12-24 16:39:04 +0800

    这个类结束

--- app/src/main/java/com/hqq/example/ui/customize/widget/ViewScrollView.java
+++ app/src/main/java/com/hqq/example/ui/customize/widget/ViewScrollView.java
@@ -1,20 +1,20 @@
 package com.hqq.example.ui.customize.widget;
 
+import android.animation.PointFEvaluator;
+import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Scroller;
 
 import androidx.annotation.Nullable;
 
 import com.hqq.core.utils.ScreenUtils;
 import com.hqq.core.utils.log.LogUtils;
-
-import java.security.acl.LastOwnerException;
 
 /**
  * @Author : huangqiqiang
@@ -22,21 +22,28 @@ import java.security.acl.LastOwnerException;
  * @FileName :   ScrollView
  * @Date : 2019/12/20 0020  下午 4:17
  * @Email : qiqiang213@gmail.com
- * @Descrive :
+ * @Descrive : 测试view 的内容滑动 以及边界判断
  */
-public class ScrollView extends View {
-    public ScrollView(Context context) {
+public class ViewScrollView extends View {
+    public ViewScrollView(Context context) {
         super(context);
+        mScroller = new Scroller(context);
+
     }
 
-    public ScrollView(Context context, @Nullable AttributeSet attrs) {
+    public ViewScrollView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        mScroller = new Scroller(context);
+
     }
 
-    public ScrollView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public ViewScrollView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        mScroller = new Scroller(context);
+
     }
 
+    Scroller mScroller;
 
     @Override
     protected void onDraw(Canvas canvas) {
@@ -55,39 +62,68 @@ public class ScrollView extends View {
 //        LogUtils.e("getMeasuredWidth()",""+getMinimumWidth());
 //        LogUtils.e("onDraw",""+ScreenUtils.dip2px(getContext(),800));
         LogUtils.e("onDraw", getScrollX());
-        LogUtils.e("getScreenWidth", "" + ScreenUtils.getScreenWidth(getContext()));
+        // LogUtils.e("getScreenWidth", "" + ScreenUtils.getScreenWidth(getContext()));
     }
 
     float distanceX = 0;
     float mKeyX;
+    float mKeyX1;
 
     @Override
     public boolean dispatchTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+                LogUtils.e("getRight", getRight());
                 LogUtils.e("ACTION_DOWN -----------" + event.getRawX());
                 mKeyX = event.getX();
+                mKeyX1 = event.getX();
                 break;
             case MotionEvent.ACTION_MOVE:
                 LogUtils.e("ACTION_MOVE -----------" + event.getRawX());
                 distanceX = mKeyX - event.getRawX();
-                scrollBy((int) distanceX, 0);
 
+
+                scrollBy((int) distanceX, 0);
                 if (getScrollX() < 0) {
                     scrollTo(0, 0);
                 } else {
                     if (getScrollX() > getWidth() - ScreenUtils.getScreenWidth(getContext())) {
                         scrollTo(getWidth() - ScreenUtils.getScreenWidth(getContext()), 0);
                     }
-
                 }
-
                 mKeyX = event.getRawX();
                 break;
             case MotionEvent.ACTION_UP:
             case MotionEvent.ACTION_CANCEL:
-                LogUtils.e("ACTION_UP -----------" + event.getRawX());
+                LogUtils.e("---------------------------------------------------------------------------------------------------------------------");
+                LogUtils.e("ACTION_CANCEL", mKeyX1 - event.getX());
                 LogUtils.e("distanceX", distanceX);
+                LogUtils.e("getRawX -----------" + event.getRawX());
+                LogUtils.e("getX", getX());
+                LogUtils.e("getLeft", getLeft());
+                LogUtils.e("getRight", getRight());
+                LogUtils.e("getTranslationX", getTranslationX());
+                LogUtils.e("getScrollX", getScrollX());
+                postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        LogUtils.e("---------------------------------------------------------------------------------------------------------------------");
+                        LogUtils.e("getScrollX", getScrollX());
+                        int getScrollX = getScrollX();
+                        ValueAnimator animator = ValueAnimator.ofInt(0, getScrollX());
+                        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                            @Override
+                            public void onAnimationUpdate(ValueAnimator animation) {
+                                int scrollX = (int) animation.getAnimatedValue();
+                                LogUtils.e("scrollX   " + scrollX);
+                                LogUtils.e("getScrollX()-scrollX   ", getScrollX - scrollX);
+                                scrollTo(getScrollX - scrollX, 0);
+                            }
+                        });
+                        animator.setDuration(5000);
+                        animator.start();
+                    }
+                }, 2 * 1000);
                 break;
             default:
 
@@ -101,18 +137,10 @@ public class ScrollView extends View {
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-
                 return true;
-            case MotionEvent.ACTION_MOVE:
-                break;
-            case MotionEvent.ACTION_UP:
-            case MotionEvent.ACTION_CANCEL:
-                break;
             default:
 
         }
-
-
         return super.onTouchEvent(event);
 
     }
