22b31bf5ab46cd31a7d18a6f47a3e7cbe9ec3933
huangqiqiang <zz789789>
2019-12-25 00:07:12 +0800

    父布局的问题

--- app/src/main/java/com/hqq/example/ui/customize/widget/RoundCountdownView.java
+++ app/src/main/java/com/hqq/example/ui/customize/widget/RoundCountdownView.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.hqq.core.utils.log.LogUtils;
+import com.hqq.example.R;
 
 /**
  * @Author : huangqiqiang
@@ -18,17 +21,22 @@ import com.hqq.core.utils.log.LogUtils;
  * @Descrive :
  */
 public class RoundCountdownView extends View {
+
+
     public RoundCountdownView(Context context) {
-        this(context, null);
+        super(context);
     }
 
     public RoundCountdownView(Context context, @Nullable AttributeSet attrs) {
-        this(context, attrs, 0);
+        super(context, attrs);
+        init(attrs);
+
     }
 
     public RoundCountdownView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs);
+
     }
 
     private void init(AttributeSet attrs) {
@@ -36,26 +44,38 @@ public class RoundCountdownView extends View {
 
         }
 
+        setBackgroundResource(R.color.color_b36d61);
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        LogUtils.e("widthMeasureSpec", widthMeasureSpec);
-        LogUtils.e("heightMeasureSpec", heightMeasureSpec);
+        if (getParent() instanceof ConstraintLayout) {
+            ViewGroup.LayoutParams params = getLayoutParams();
+            if (params!=null) {
+                params.height=0;
+                params.width=0;
+            }else {
+                params=new ViewGroup.LayoutParams(0,0);
+            }
+            setLayoutParams(params);
+        }
         int wSpecMode = MeasureSpec.getMode(widthMeasureSpec);
         int wSpecSize = MeasureSpec.getSize(widthMeasureSpec);
         int hSpecMode = MeasureSpec.getMode(heightMeasureSpec);
         int hSpecSize = MeasureSpec.getSize(heightMeasureSpec);
-
-        if (wSpecMode == MeasureSpec.AT_MOST && hSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(300, 300);
-        } else if (wSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(300, hSpecSize);
-        } else if (hSpecMode == MeasureSpec.AT_MOST) {
-            setMeasuredDimension(wSpecSize, 300);
-        }
+        LogUtils.e("wSpecSize", wSpecSize);
+//        if (wSpecMode == MeasureSpec.AT_MOST && hSpecMode == MeasureSpec.AT_MOST) {
+//            setMeasuredDimension(300, 300);
+//        } else if (wSpecMode == MeasureSpec.AT_MOST) {
+//            setMeasuredDimension(300, hSpecSize);
+//        } else if (hSpecMode == MeasureSpec.AT_MOST) {
+//            setMeasuredDimension(300, 300);
+//        }
+        wSpecSize = 300;
+        setMeasuredDimension(wSpecSize, 300);
+        LogUtils.e("onMeasure getWidth", getMeasuredWidth());
 
 
     }
@@ -63,6 +83,8 @@ public class RoundCountdownView extends View {
     @Override
     public void draw(Canvas canvas) {
         super.draw(canvas);
+
+        LogUtils.e("getWidth", getWidth());
     }
 
 }
