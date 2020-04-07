ef9550b4280a59cf792dc13bad9741713a8bc4e2
huangqiqiang <zz789789>
2020-04-07 11:48:31 +0800

    添加一个黑白化的解决方案

--- app/src/main/java/com/hqq/example/ui/view/BlackAndWhiteActivity.java
+++ app/src/main/java/com/hqq/example/ui/view/BlackAndWhiteActivity.java
@@ -8,6 +8,15 @@ import com.hqq.core.ui.BaseActivity;
 import com.hqq.core.utils.BaseCommonUtils;
 import com.hqq.example.R;
 
+/**
+ * @version V1.0 <描述当前版本功能>
+ * @Author : huangqiqiang
+ * @Package : com.hqq.example.ui.view
+ * @FileName :   BlackAndWhiteActivity
+ * @Date : 2020/4/7  上午11:35
+ * @Email : qiqiang213@gmail.com
+ * @Descrive :  黑白化
+ */
 public class BlackAndWhiteActivity extends BaseActivity {
     public static void open(Context context) {
         Intent starter = new Intent(context, BlackAndWhiteActivity.class);
