22827645431fa2d391f88e4a23bb25334bd62607
593979591@qq.com <zz789789>
2021-01-12 09:43:17 +0800

    M

--- Core/src/main/java/com/hqq/core/lifecycle/BaseService.kt
+++ Core/src/main/java/com/hqq/core/lifecycle/BaseService.kt
@@ -1,13 +1,11 @@
-package com.hqq.core.lifecycle;
+package com.hqq.core.lifecycle
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
-import androidx.annotation.NonNull;
-import androidx.lifecycle.Lifecycle;
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.LifecycleRegistry;
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
 
 /**
  * @Author : huangqiqiang
@@ -16,44 +14,34 @@ import androidx.lifecycle.LifecycleRegistry;
  * @Email : qiqiang213@gmail.com
  * @Describe :
  */
-public abstract class BaseService extends Service  implements LifecycleOwner {
-     LifecycleRegistry mLifecycleRegistry = new LifecycleRegistry(this);
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE);
+abstract class BaseService : Service(), LifecycleOwner {
+    var mLifecycleRegistry = LifecycleRegistry(this)
+    override fun onCreate() {
+        super.onCreate()
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_CREATE)
     }
 
-
-    @Override
-    public void onStart(Intent intent, int startId) {
-        super.onStart(intent, startId);
-        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START);
+    override fun onStart(intent: Intent, startId: Int) {
+        super.onStart(intent, startId)
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_START)
     }
 
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME);
-        return null;
+    override fun onBind(intent: Intent): IBinder? {
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_RESUME)
+        return null
     }
 
-    @Override
-    public boolean onUnbind(Intent intent) {
-        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP);
-        return super.onUnbind(intent);
+    override fun onUnbind(intent: Intent): Boolean {
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_STOP)
+        return super.onUnbind(intent)
     }
 
-    @Override
-    public void onDestroy() {
-        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY);
-        super.onDestroy();
+    override fun onDestroy() {
+        mLifecycleRegistry.handleLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+        super.onDestroy()
     }
 
-    @NonNull
-    @Override
-    public Lifecycle getLifecycle() {
-        return mLifecycleRegistry;
+    override fun getLifecycle(): Lifecycle {
+        return mLifecycleRegistry
     }
-
 }
