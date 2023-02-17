22b56e3ba2aa98190667814f91b584aa285749f9
593979591@qq.com <593979591@qq.com>
2023-02-17 11:32:45 +0800

    添加阿里支付

---
+++ ALiBaBa/src/main/java/com/hqq/alibaba/interfaces/OnSuccessAndErrorListener.kt
@@ -0,0 +1,10 @@
+package com.hqq.alibaba.interfaces
+
+/**
+ * @author Tamsiree
+ * @date 2017/4/19
+ */
+interface OnSuccessAndErrorListener {
+    fun onSuccess(s: String?)
+    fun onError(s: String?)
+}
