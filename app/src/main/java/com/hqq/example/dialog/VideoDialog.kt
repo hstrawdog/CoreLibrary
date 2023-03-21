ddbf4dec6b68f45f5608dba9bb3180032a47fe9f
593979591@qq.com <593979591@qq.com>
2023-03-21 15:21:55 +0800

    VideoView  圆角

--- app/src/main/java/com/hqq/example/dialog/VideoDialog.kt
+++ app/src/main/java/com/hqq/example/dialog/VideoDialog.kt
@@ -1,7 +1,12 @@
 package com.hqq.example.dialog
 
+import android.media.MediaPlayer
+import android.net.Uri
+import android.widget.MediaController
+import com.hqq.core.ui.dialog.BaseBindingDialog
 import com.hqq.core.ui.dialog.BaseDialog
 import com.hqq.example.R
+import com.hqq.example.databinding.DialogVideoBinding
 
 /**
  * @Author : huangqiqiang
@@ -10,11 +15,17 @@ import com.hqq.example.R
  * @Email : qiqiang213@gmail.com
  * @Describe :
  */
-class VideoDialog : BaseDialog() {
-    override val layoutId: Int = R.layout.dialog_video
+class VideoDialog : BaseBindingDialog<DialogVideoBinding>() {
     override fun initView() {
 
-
+        val uri =
+            Uri.parse("https://vlogtest.shangwenwan.com/mall/64cf2a3f-b049-4d98-ae68-ec5cbaa8dc64")
+        binding.videoView.setVideoURI(uri)
+        binding.videoView.setMediaController(MediaController(activity))
+        binding.videoView.setOnPreparedListener(MediaPlayer.OnPreparedListener { mp ->
+            mp.isLooping = true
+        })
+        binding.videoView.start()
     }
 
 }
