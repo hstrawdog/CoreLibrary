e9e4328c266d0496c0a016405920dd00cc443d1a
593979591@qq.com <593979591@qq.com>
2024-03-02 22:25:19 +0800

    1

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -6,3 +6,9 @@ repositories{
     google()
     gradlePluginPortal()
 }
+dependencies {
+//    implementation("com.android.tools.build:gradle:7.0.2")
+//    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.0")
+    implementation(gradleApi())
+    implementation(localGroovy())
+}
