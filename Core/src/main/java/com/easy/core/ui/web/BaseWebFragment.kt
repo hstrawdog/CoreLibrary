e8d8b09a37790e553903870ffd4ffe42a9500010
593979591@qq.com <593979591@qq.com>
2024-03-08 13:29:30 +0800

    修改webView

--- Core/src/main/java/com/easy/core/ui/web/BaseWebFragment.kt
+++ Core/src/main/java/com/easy/core/ui/web/BaseWebFragment.kt
@@ -7,7 +7,9 @@ import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.view.ViewGroup
 import android.webkit.*
 import android.widget.ProgressBar
 import com.easy.core.R
@@ -16,6 +18,7 @@ import com.easy.core.listenner.WebLoadListener
 import com.easy.core.ui.base.BaseFragment
 import com.easy.core.utils.AppTool
 import com.easy.core.utils.DataUtils
+import com.easy.core.utils.log.LogUtils
 
 /**
  * @Author : huangqiqiang
@@ -67,6 +70,7 @@ open class BaseWebFragment : BaseFragment() {
     override fun getLayoutViewId(): Int {
         return  R.layout.fragment_web
     }
+
     /**
      *  标题栏
      */
@@ -92,6 +96,11 @@ open class BaseWebFragment : BaseFragment() {
             }
         }
 
+        override fun onShowCustomView(view: View?, callback: CustomViewCallback?) {
+            super.onShowCustomView(view, callback)
+
+        }
+
         override fun onProgressChanged(view: WebView, newProgress: Int) {
             if (newProgress >= 5) {
                 progressBar?.progress = newProgress
@@ -113,10 +122,9 @@ open class BaseWebFragment : BaseFragment() {
 
     override fun initConfig() {
         super.initConfig()
-        rootViewImpl.iToolBarBuilder.showToolBar =
-            arguments?.getBoolean(getString(R.string.key_showToolBar), true) == true
-        rootViewImpl.iToolBarBuilder.showStatusBar =
-            arguments?.getBoolean(getString(R.string.key_showstatusBar), true) == true
+        rootViewImpl.iToolBarBuilder.showToolBar = arguments?.getBoolean(getString(R.string.key_showToolBar), true) == true
+        rootViewImpl.iToolBarBuilder.showLine = arguments?.getBoolean(getString(R.string.key_showToolBar), true) == true
+        rootViewImpl.iToolBarBuilder.showStatusBar = arguments?.getBoolean(getString(R.string.key_showstatusBar), true) == true
     }
 
     @SuppressLint("JavascriptInterface")
@@ -128,18 +136,26 @@ open class BaseWebFragment : BaseFragment() {
                 progressBar?.indeterminateTintList = progressBarColor
             }
         }
-        progressBar?.max = 100
+
         //先加载5%，以使用户觉得界面没有卡死
+        progressBar?.max = 100
         progressBar?.progress = 5
         initWebViewSettings()
         webView?.webViewClient = webViewClient
-        webView?.webChromeClient = webChromeClient
+
         // 标识 为Android的 js 支持 对象是activity
-        if (scriptInterface != null) {
-            webView?.addJavascriptInterface(requireActivity(), "android")
+        webView?.webChromeClient = webChromeClient
+
+        webView?.let {
+            it.setWebChromeClient(InsideWebChromeClient(it, it.parent as ViewGroup))
+
+        }
+        scriptInterface?.let {
+            webView?.addJavascriptInterface(it, "android")
         }
         url = getString(getString(R.string.key_url))
         title =   getString(getString(R.string.key_title))
+
         requireActivity().title = title
         iToolBar?.setToolbarTitle(title)
         url?.let {
@@ -158,17 +174,16 @@ open class BaseWebFragment : BaseFragment() {
             loadWithOverviewMode = true
             builtInZoomControls = true
             domStorageEnabled = true
+            cacheMode = WebSettings.LOAD_NO_CACHE
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
                 displayZoomControls = false
-            }
-            // MIXED_CONTENT_ALWAYS_ALLOW：允许从任何来源加载内容，即使起源是不安全的；
+            } // MIXED_CONTENT_ALWAYS_ALLOW：允许从任何来源加载内容，即使起源是不安全的；
             // MIXED_CONTENT_NEVER_ALLOW：不允许Https加载Http的内容，即不允许从安全的起源去加载一个不安全的资源；
             // MIXED_CONTENT_COMPATIBILITY_MODE：当涉及到混合式内容时，WebView 会尝试去兼容最新Web浏览器的风格。
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                 mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
             }
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) {
-                //提高渲染优先级
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR2) { //提高渲染优先级
                 setRenderPriority(WebSettings.RenderPriority.HIGH)
                 savePassword = false
                 pluginState = WebSettings.PluginState.ON
@@ -176,27 +191,60 @@ open class BaseWebFragment : BaseFragment() {
         }
     }
 
+    private class InsideWebChromeClient(var webView: WebView, var mFrameLayout: ViewGroup) : WebChromeClient() {
+        var mCustomView: View? = null
+        var mCustomViewCallback: CustomViewCallback? = null
+
+        override fun getVideoLoadingProgressView(): View? {
+            return super.getVideoLoadingProgressView()
+        }
+
+        override fun onShowCustomView(view: View, callback: CustomViewCallback) {
+            super.onShowCustomView(view, callback)
+            if (mCustomView != null) {
+                callback.onCustomViewHidden()
+                return
+            }
+            mCustomView = view
+            mFrameLayout.addView(mCustomView)
+            mCustomViewCallback = callback
+            webView.setVisibility(View.GONE)
+            Handler().postDelayed({
+                val js = "javascript: var v=document.getElementsByTagName('video')[0]; " + "v.play(); "
+                webView.loadUrl(js)
+            }, 100)
+        }
+
+        override fun onHideCustomView() {
+            webView.setVisibility(View.VISIBLE)
+            if (mCustomView == null) {
+                return
+            }
+            mCustomView?.setVisibility(View.GONE)
+            mFrameLayout.removeView(mCustomView)
+            mCustomViewCallback!!.onCustomViewHidden()
+            mCustomView = null
+            super.onHideCustomView()
+        }
+    }
+
     //处理 android err_unknown_url_scheme异常
-    private val webViewClient: WebViewClient = object : WebViewClient() {
+    val webViewClient: WebViewClient = object : WebViewClient() {
         override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
-            //处理 android err_unknown_url_scheme异常
+            LogUtils.e(" shouldOverrideUrlLoading :    ${url.toString()}")
             if (URLUtil.isNetworkUrl(request.url.toString())) {
                 return false
             }
             if (AppTool.appInstalledOrNot(activity, request.url.toString())) {
                 val intent = Intent(Intent.ACTION_VIEW, request.url)
                 startActivity(intent)
-            } else {
-                // do something if app is not installed
+            } else { // do something if app is not installed
             }
             return super.shouldOverrideUrlLoading(view, request)
         }
 
-        override fun shouldInterceptRequest(
-            view: WebView, request: WebResourceRequest
-        ): WebResourceResponse? {
-            return super.shouldInterceptRequest(view, request)
-            //request.getUrl()
+        override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
+            return super.shouldInterceptRequest(view, request) //request.getUrl()
         }
 
         override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
@@ -215,22 +263,18 @@ open class BaseWebFragment : BaseFragment() {
             }
         }
 
-        override fun onReceivedError(
-            view: WebView, errorCode: Int, description: String?, failingUrl: String?
-        ) {
-            super.onReceivedError(view, errorCode, description, failingUrl)
-            // 断网或者网络连接超时
+        override fun onReceivedError(view: WebView, errorCode: Int, description: String?, failingUrl: String?) {
+            super.onReceivedError(view, errorCode, description, failingUrl) // 断网或者网络连接超时
             if (errorCode == ERROR_HOST_LOOKUP || errorCode == ERROR_CONNECT || errorCode == ERROR_TIMEOUT) {
                 view.loadUrl("about:blank");// 避免出现默认的错误界面
                 webLoadListener?.onError(failingUrl)
             }
         }
 
-        override fun onReceivedError(
-            view: WebView, request: WebResourceRequest, error: WebResourceError
-        ) {
+        override fun onReceivedError(view: WebView, request: WebResourceRequest, error: WebResourceError) {
             super.onReceivedError(view, request, error)
             progressBar?.visibility = View.GONE
+
         }
     }
 
@@ -260,14 +304,12 @@ open class BaseWebFragment : BaseFragment() {
     }
 
     companion object {
-        fun instantiate(
-            context: Context,
-            title: String?,
-            url: String?,
-            scriptInterface: ScriptInterface? = null,
-            showToolBar: Boolean = true,
-            showstatusBar: Boolean = true
-        ): BaseWebFragment {
+        fun instantiate(context: Context,
+                        title: String?,
+                        url: String?,
+                        scriptInterface: ScriptInterface? = null,
+                        showToolBar: Boolean = true,
+                        showstatusBar: Boolean = true): BaseWebFragment {
             val baseWebFragment = BaseWebFragment()
             val bundle = Bundle()
             bundle.putString(context.getString(R.string.key_url), url)
@@ -279,4 +321,4 @@ open class BaseWebFragment : BaseFragment() {
             return baseWebFragment
         }
     }
-}
+};
