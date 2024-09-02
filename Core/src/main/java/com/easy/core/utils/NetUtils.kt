771178d487b0b17989cc81fb0b11ddd85130a446
593979591@qq.com <593979591@qq.com>
2024-09-02 11:48:37 +0800

    修改 网络 状态

--- Core/src/main/java/com/easy/core/utils/NetUtils.kt
+++ Core/src/main/java/com/easy/core/utils/NetUtils.kt
@@ -9,6 +9,7 @@ import android.net.NetworkInfo
 import android.os.Build
 import android.provider.Settings
 import android.telephony.TelephonyManager
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import java.io.BufferedReader
 import java.io.IOException
@@ -75,7 +76,7 @@ object NetUtils {
      * @link #NETWORK_UNKNOWN =  6; 未知网络
      */
     @JvmStatic
-    fun getNetWorkType(context: Context): Int {
+    fun getNetWorkType(context: Context , showToast: Boolean =false): Int {
         // 获取ConnectivityManager
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val ni = cm.activeNetworkInfo // 获取当前网络状态
@@ -84,27 +85,32 @@ object NetUtils {
             when (ni.type) {
                 ConnectivityManager.TYPE_WIFI -> {
                     netType = NETWORK_WIFI
+                    if (showToast)
                     ToastUtils.showToast("切换到wifi环境下")
                 }
 
                 ConnectivityManager.TYPE_MOBILE -> when (ni.subtype) {
                     NETWORK_TYPE_GSM, TelephonyManager.NETWORK_TYPE_GPRS, TelephonyManager.NETWORK_TYPE_CDMA, TelephonyManager.NETWORK_TYPE_EDGE, TelephonyManager.NETWORK_TYPE_1xRTT, TelephonyManager.NETWORK_TYPE_IDEN -> {
                         netType = NETWORK_2G
+                        if (showToast)
                         ToastUtils.showToast("切换到2G环境下")
                     }
 
                     TelephonyManager.NETWORK_TYPE_EVDO_A, TelephonyManager.NETWORK_TYPE_UMTS, TelephonyManager.NETWORK_TYPE_EVDO_0, TelephonyManager.NETWORK_TYPE_HSDPA, TelephonyManager.NETWORK_TYPE_HSUPA, TelephonyManager.NETWORK_TYPE_HSPA, TelephonyManager.NETWORK_TYPE_EVDO_B, TelephonyManager.NETWORK_TYPE_EHRPD, TelephonyManager.NETWORK_TYPE_HSPAP, NETWORK_TYPE_TD_SCDMA -> {
                         netType = NETWORK_3G
+                        if (showToast)
                         ToastUtils.showToast("切换到3G环境下")
                     }
 
                     TelephonyManager.NETWORK_TYPE_LTE, NETWORK_TYPE_IWLAN -> {
                         netType = NETWORK_4G
+                        if (showToast)
                         ToastUtils.showToast("切换到4G环境下")
                     }
 
                     TelephonyManager.NETWORK_TYPE_NR -> {
                         netType = NETWORK_5G
+                        if (showToast)
                         ToastUtils.showToast("切换到5G环境下")
                     }
 
@@ -117,17 +123,20 @@ object NetUtils {
                         } else {
                             NETWORK_UNKNOWN
                         }
+                        if (showToast)
                         ToastUtils.showToast("未知网络")
                     }
                 }
 
                 else -> {
                     netType = 5
+                    if (showToast)
                     ToastUtils.showToast("未知网络")
                 }
             }
         } else {
             netType = NETWORK_NO
+            if (showToast)
             ToastUtils.showToast(context, "当前无网络连接")
         }
         return netType
