b3df6f75f5ce126050a715b2fa9ddfffdfa0f5b4
hqq <593979591@qq.com>
2026-08-11 16:11:40 +0800

    1

--- Core/src/main/java/com/easy/core/ui/base/BaseViewBindingActivity.kt
+++ Core/src/main/java/com/easy/core/ui/base/BaseViewBindingActivity.kt
@@ -23,7 +23,7 @@ abstract class BaseViewBindingActivity<T : ViewBinding> : BaseActivity() {
     lateinit var binding: T
 
     override fun getLayoutView(parent: ViewGroup): View? {
-        val tClass = (javaClass.genericSuperclass as ParameterizedType).actualTypeArguments[0] as Class<T>
+        val tClass = findViewBindingClass()
         try {
             val method = tClass.getMethod("inflate", LayoutInflater::class.java, ViewGroup::class.java, Boolean::class.javaPrimitiveType)
             binding = method.invoke(null, layoutInflater, parent, false) as T
@@ -38,5 +38,29 @@ abstract class BaseViewBindingActivity<T : ViewBinding> : BaseActivity() {
             LogUtils.dInfo{e}
         }
         return binding?.root
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun findViewBindingClass(): Class<T> {
+        var currentClass: Class<*> = javaClass
+        while (currentClass != Any::class.java) {
+            val genericSuperclass = currentClass.genericSuperclass
+            if (genericSuperclass is ParameterizedType) {
+                val rawClass = genericSuperclass.rawType as? Class<*> ?: break
+                if (rawClass == BaseViewBindingActivity::class.java) {
+                    val bindingClass = genericSuperclass.actualTypeArguments.firstOrNull() as? Class<*>
+                    if (bindingClass != null && ViewBinding::class.java.isAssignableFrom(bindingClass)) {
+                        return bindingClass as Class<T>
+                    }
+                    break
+                }
+                currentClass = rawClass
+            } else if (genericSuperclass is Class<*>) {
+                currentClass = genericSuperclass
+            } else {
+                break
+            }
+        }
+        throw IllegalStateException("Unable to resolve ViewBinding type for ${javaClass.name}")
     }
 }
